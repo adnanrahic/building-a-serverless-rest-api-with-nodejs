d2b795f94ff4d2afac61ccbd9ba61c32e29f7d0e
adnanrahic <ado.raha198@gmail.com>
2017-12-27 22:26:24 +0100

    (features) added connect to database function; added create report function; set dotenv; added bluebird; wrapped all functions in connect wrappers; added callback event loop wait;

--- handler.js
+++ handler.js
@@ -1,13 +1,36 @@
 'use strict';
 
 require('dotenv').config({ path: './variables.env' });
+const bluebird = require('bluebird');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB, { useMongoClient: true });
-mongoose.Promise = global.Promise;
+mongoose.Promise = bluebird;
+let cachedDb;
 const Note = mongoose.model('Note', { content: String });
+const Report = mongoose.model('Report', { content: String, env: String, createdAt: { type: Date, default: Date.now } });
+
+function createReport(env) {
+  Report.create({ content: 'Connection request', env: env });
+}
+
+function connectToDatabase() {
+  if (cachedDb) {
+    console.log('=> using cached database instance');
+    createReport('using cached database instance');
+    return Promise.resolve();
+  }
+
+  console.log('=> using new database instance');
+  createReport('using new database instance');
+  return mongoose.connect(process.env.DB, { useMongoClient: true })
+    .then(db => { cachedDb = db; });
+}
 
 module.exports.create = (event, context, callback) => {
-  Note.create(JSON.parse(event.body))
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase()
+    .then(() =>
+      Note.create(JSON.parse(event.body))
+    )
     .then(note => callback(null, {
       statusCode: 200,
       body: JSON.stringify(note)
@@ -20,7 +43,11 @@ module.exports.create = (event, context, callback) => {
 }
 
 module.exports.getOne = (event, context, callback) => {
-  Note.findById(event.pathParameters.id)
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase()
+    .then(() =>
+      Note.findById(event.pathParameters.id)
+    )
     .then(note => callback(null, {
       statusCode: 200,
       body: JSON.stringify(note)
@@ -33,7 +60,11 @@ module.exports.getOne = (event, context, callback) => {
 };
 
 module.exports.getAll = (event, context, callback) => {
-  Note.find()
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase()
+    .then(() =>
+      Note.find()
+    )
     .then(notes => callback(null, {
       statusCode: 200,
       body: JSON.stringify(notes)
@@ -42,11 +73,15 @@ module.exports.getAll = (event, context, callback) => {
       statusCode: err.statusCode || 500,
       headers: { 'Content-Type': 'text/plain' },
       body: 'Could not fetch the notes.'
-    }));
+    }))
 };
 
 module.exports.update = (event, context, callback) => {
-  Note.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase()
+    .then(() =>
+      Note.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+    )
     .then(note => callback(null, {
       statusCode: 200,
       body: JSON.stringify(note)
@@ -59,7 +94,11 @@ module.exports.update = (event, context, callback) => {
 };
 
 module.exports.delete = (event, context, callback) => {
-  Note.findByIdAndRemove(event.pathParameters.id)
+  context.callbackWaitsForEmptyEventLoop = false;
+  return connectToDatabase()
+    .then(() =>
+      Note.findByIdAndRemove(event.pathParameters.id)
+    )
     .then(note => callback(null, {
       statusCode: 200,
       body: JSON.stringify({ message: 'Removed note with id: ' + note._id, note: note })
